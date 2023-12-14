2ac1fcfdcd659a8f0797522749dbe5634246b6bd
shirinox <cosminsebastian2004@gmail.com>
2023-12-14 20:47:05 +0200

    Fixed fonts'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,34 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
-import Announcement from './_components/Announcement';
 
-const inter = Inter({ subsets: ['latin'] });
+import Announcement from './_components/Announcement';
+import Navigation from './_components/Navigation';
+
+const basier = localFont({
+	src: [
+		{
+			path: './fonts/basiersquare-bold-webfont.woff2',
+			weight: '700',
+			style: 'normal',
+		},
+		{
+			path: './fonts/basiersquare-medium-webfont.woff2',
+			weight: '500',
+			style: 'normal',
+		},
+		{
+			path: './fonts/basiersquare-regular-webfont.woff2',
+			weight: '400',
+			style: 'normal',
+		},
+		{
+			path: './fonts/basiersquare-regularitalic-webfont.woff2',
+			weight: '400',
+			style: 'italic',
+		},
+	],
+});
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -13,8 +38,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className={basier.className}>
 				<Announcement message='Programul nostru de sarbatori' />
+				<Navigation />
 				{children}
 			</body>
 		</html>
