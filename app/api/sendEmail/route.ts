495743191f1951e39fa1cc7cb396aaefa7414e55
shirinox <cosminsebastian2004@gmail.com>
2023-12-16 03:17:14 +0200

    Added backend contact form checks

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -3,19 +3,21 @@ import { Resend } from 'resend';
 import { EmailTemplate } from '@/app/_components/email-template';
 import { ReactElement } from 'react';
 
-const resend = new Resend(process.env.RESEND_SECRET);
+import { ZodFormData } from '@/app/_components/EmailForm';
 
-type FormData = {
-	name: string;
-	email: string;
-	subject: string;
-	message: string;
-};
+const resend = new Resend(process.env.RESEND_SECRET);
 
 export async function POST(req: NextRequest) {
 	try {
 		const body = JSON.stringify(req.body);
-		const { name, email, subject, message } = JSON.parse(body) as FormData;
+		const formData = ZodFormData.safeParse(body);
+		if (!formData.success)
+			return NextResponse.json(
+				'Datele trimise nu sunt valide. Contactati un administrator daca credeti ca aceasta a fost o greseala.'
+			);
+		const {
+			data: { name, email, subject, message },
+		} = formData;
 		const res = await resend.emails.send({
 			from: `${name} <${email}>`,
 			to: ['contact@galagym.ro'],
